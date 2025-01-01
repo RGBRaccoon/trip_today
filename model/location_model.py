6958823848e96f9c08d91e31dde7ea956957d796
RGB_Raccoon <chldusgh0497@gmail.com>
2025-01-01 14:28:58 +0900

    create model

--- model/location_model.py
+++ model/location_model.py
@@ -1,5 +1,8 @@
+from sqlalchemy import JSON, Integer
 from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import mapped_column, Mapped
 
 
 class LocationModel(DeclarativeBase):
-    pass
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    location_list: Mapped[int] = mapped_column(JSON, primary_key=True, index=True)
