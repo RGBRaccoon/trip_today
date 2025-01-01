aedf20fd060d2120af3b44f088e9b8800e4b7829
RGB_Raccoon <chldusgh0497@gmail.com>
2025-01-01 15:40:20 +0900

    base repository

--- core/base_service.py
+++ core/base_service.py
@@ -4,3 +4,8 @@ from sqlalchemy.ext.asyncio import AsyncSession
 class BaseService:
     def __init__(self, session: AsyncSession):
         self.session = session
+
+
+class BaseRepository:
+    def __init__(self, session: AsyncSession):
+        self.session = session
